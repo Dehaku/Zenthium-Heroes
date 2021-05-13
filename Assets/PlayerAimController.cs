60d6d47ea604015e47ab8455fb98249ee625383b
Dehaku <dehakuzedon@gmail.com>
2021-05-13 13:34:58 -0500

    Improved camera massively

--- Assets/PlayerAimController.cs
+++ Assets/PlayerAimController.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerAimController : MonoBehaviour
 {
     public CharacterController controller;
 
-    public GameObject mainCamera;
     public GameObject aimCamera;
+    public Transform aimLookAt;
     public GameObject aimReticle;
+    public float sensitivity = 1;
+
+    public Cinemachine.AxisState xAxis;
+    public Cinemachine.AxisState yAxis;
+
+    public float camMainDistance = 10f;
+    public float camShoulderDistance = 4.5f;
+    float _camTransitDistance;
+
+    public Vector3 camMainOffset = new Vector3(1.5f, 0, 0);
+    public Vector3 camShoulderOffset = new Vector3(1.5f, 0, 0);
+    Vector3 _camTransitOffset;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camTransitDistance = camMainDistance;
+        _camTransitOffset = camMainOffset;
     }
 
    
@@ -21,24 +37,76 @@ public class PlayerAimController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+
+
+
+
+        
+        
+
+        xAxis.Update(Time.deltaTime);
+        yAxis.Update(Time.deltaTime);
+
+        aimLookAt.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
+
+        var aim = aimCamera.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+
         if (Input.GetMouseButtonDown(1))
         {
-            
-            
             controller.transform.rotation = Quaternion.LookRotation(controller.transform.position, Camera.main.transform.up);
             var lookPosition = controller.transform.position + Camera.main.transform.forward;
-            
+
             controller.transform.LookAt(lookPosition);
-          
-            mainCamera.SetActive(false);
-            aimCamera.SetActive(true);
-            aimCamera.transform.position = mainCamera.transform.position;
         }
-        else if (Input.GetMouseButtonUp(1))
+
+        if (Input.GetMouseButton(1))
         {
-            mainCamera.SetActive(true);
-            aimCamera.SetActive(false);
-            mainCamera.transform.position = aimCamera.transform.position;
+            //aim.CameraDistance = camShoulderDistance;
+            aim.CameraDistance = Mathf.Lerp(aim.CameraDistance, camShoulderDistance, Time.deltaTime * 4);
+            //aim.ShoulderOffset = camShoulderOffset;
+            aim.ShoulderOffset = Vector3.Lerp(aim.ShoulderOffset, camShoulderOffset, Time.deltaTime * 4);
+
+            xAxis.m_MaxSpeed = 100;
+            yAxis.m_MaxSpeed = 100;
+        }
+        else
+        {
+            //aim.CameraDistance = camMainDistance;
+            aim.CameraDistance = Mathf.Lerp(aim.CameraDistance, camMainDistance, Time.deltaTime * 4);
+            //aim.ShoulderOffset = camMainOffset;
+            aim.ShoulderOffset = Vector3.Lerp(aim.ShoulderOffset, camMainOffset, Time.deltaTime * 4);
+            xAxis.m_MaxSpeed = 500;
+            yAxis.m_MaxSpeed = 300;
+
+            var mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+            if (mouseScroll < 0)
+                camMainDistance += 3;
+            if (mouseScroll > 0)
+                camMainDistance -= 3;
+
+            camMainDistance = Mathf.Clamp(camMainDistance,camShoulderDistance,20);
+            
+
+        }
+            
+
+        if (aimCamera.activeSelf)
+        {
+            float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+            float horizontal = Input.GetAxis("Mouse X") * sensitivity;
+
+            
+            
+
+            // aimLookAt.transform.Rotate(new Vector3(vertical,horizontal, -aimLookAt.transform.rotation.z));
+
+            
+
+            //aimLookAt.transform.position = new Vector3(aimLookAt.transform.position.x, aimLookAt.transform.position.y+vertical, aimLookAt.transform.position.z);
+
+
         }
     }
 }
--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -26,8 +26,26 @@ public class ThirdPersonMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OriginalMove()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    void OldMove()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -43,17 +61,26 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             Vector3 moveTransition = new Vector3(moveDir.normalized.x * speed, _movement.y, moveDir.normalized.z * speed);
-            
+
             _movement = moveTransition;
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
             _movement.y = jumpSpeed;
 
         if (gravity && !controller.isGrounded)
             _movement.y -= _gravity * Time.deltaTime;
 
         controller.Move(_movement * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // OriginalMove();
+        OldMove();
+        
+
     }
 }
