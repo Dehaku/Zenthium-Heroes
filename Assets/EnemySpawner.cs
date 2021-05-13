a638047254719096788076aa6594fbc3d32e8448
Dehaku <dehakuzedon@gmail.com>
2021-05-13 16:42:24 -0500

    Respawning and cleaned container

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public bool allowSpawning = false;
+    public bool allowRespawning = true;
     public Vector3 spawnCenter;
     public float spawnRadius;
     public int maxEnemiesAtOnce;
 
     public GameObject enemyPrefab;
     //[HideInInspector]
-    public GameObject[] enemyContainer;
+    public Queue<GameObject> enemyContainer = new Queue<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyContainer = new GameObject[maxEnemiesAtOnce];
+    }
+
+    void RespawnDeadEnemies(bool oneAtATime = false)
+    {
+        foreach (var enemy in enemyContainer)
+        {
+            if(enemy.activeSelf == false)
+            {
+                Vector3 spawnPos = spawnCenter + new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
+                enemy.transform.position = spawnPos;
+                enemy.SetActive(true);
+                return;
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -27,16 +42,34 @@ public class EnemySpawner : MonoBehaviour
             allowSpawning = true;
         }
 
+        if (allowRespawning)
+            RespawnDeadEnemies(true);
+
         if (allowSpawning)
         {
             for (int i = 0; i < maxEnemiesAtOnce; i++)
             {
                 Vector3 spawnPos = spawnCenter + new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
                 GameObject enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity, transform);
-                enemyContainer[i] = enemy;
+                //enemyContainer[i] = enemy;
+                enemyContainer.Enqueue(enemy);
             }
             allowSpawning = false;
             
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("EnemyContainer: " + enemyContainer.Count);
+
+            foreach (var enemy in enemyContainer)
+            {
+                
+                
+                if (Random.Range(0, 2) == 1)
+                    enemy.SetActive(false);
+            }
         }
     }
 }
