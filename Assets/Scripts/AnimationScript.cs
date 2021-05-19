43ece73c477da989690adb18c38ec24ae9f4778b
Dehaku <dehakuzedon@gmail.com>
2021-05-19 15:09:25 -0500

    Player gained Model

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -5,9 +5,10 @@ using UnityEngine.AI;
 
 public class AnimationScript : MonoBehaviour
 {
-    public Animator Animator { get; private set; }
+    public Animator animator { get; private set; }
     Rigidbody rigid;
     NavMeshAgent navAgent;
+    CharacterController cc;
 
     public float speed = 0f;
     public float divideBy = 0.5f;
@@ -15,23 +16,29 @@ public class AnimationScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Animator = GetComponentInChildren<Animator>();
+        animator = GetComponentInChildren<Animator>();
         rigid = GetComponent<Rigidbody>();
         navAgent = GetComponent<NavMeshAgent>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(navAgent != null)
+        if (cc != null)
+            speed = Mathf.Lerp(speed, cc.velocity.normalized.magnitude, Time.deltaTime * 10);
+
+        if (animator != null && cc != null)
+            animator.SetFloat("Speed", speed);
+
+        if (navAgent != null)
         {
             if(navAgent.enabled)
             {
                 var vel = (navAgent.velocity.magnitude + 0.001f);
                 speed = vel;
 
-                Animator.SetFloat("Speed", vel);
+                animator.SetFloat("Speed", vel);
             }
             
         }
