5643ec3e3fb481d805aeb011b73cc5dc73fa799f
Dehaku <dehakuzedon@gmail.com>
2021-09-14 15:59:37 -0500

    Settable Colors for meshes

--- Assets/ShaderController.cs
+++ Assets/ShaderController.cs
@@ -2,39 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MaterialHolder
+{
+    [SerializeField]
+    public Material mat;
+    [SerializeField]
+    public float health;
+    [SerializeField]
+    public Color color;
+
+}
+
+
 public class ShaderController : MonoBehaviour
 {
+    public bool autoUpdateSettings = false;
     public bool updateSettings = false;
     public SkinnedMeshRenderer rendererz;
-    public Material mat;
+    //private Material mat;
     public float health;
     public Color color;
 
+    [SerializeField]
+    public List<MaterialHolder> mats = new List<MaterialHolder>();
 
-
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        //var renderer = GetComponent<MeshRenderer>();
-        mat = Instantiate(rendererz.sharedMaterial);
-        rendererz.material = mat;
 
-        //previousColor = material.GetColor("_BaseColor");
-
+        SkinnedMeshRenderer[] skins = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var skin in skins)
+        {
+            Material[] matArray = skin.materials;
+            int inter = 0;
+            foreach (var matIndiv in matArray)
+            {
+                MaterialHolder matHold = new MaterialHolder();
+                matHold.mat = Instantiate(matIndiv);
+                matArray[inter] = matHold.mat;
+                mats.Add(matHold);
+                inter++;
+            }
+            skin.materials = matArray;
+        }
     }
 
     private void OnDestroy()
     {
-        if(mat != null)
+        foreach (var matHold in mats)
         {
-            Destroy(mat);
+            if (matHold.mat != null)
+            {
+                Destroy(matHold.mat);
+            }
         }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (autoUpdateSettings)
+            updateSettings = true;
+
         if(updateSettings)
         {
             updateSettings = false;
@@ -43,6 +76,11 @@ public class ShaderController : MonoBehaviour
     }
     void UpdateSettings()
     {
-        mat.SetFloat("Dissolve", health);
+        foreach (var matHold in mats)
+        {
+            matHold.mat.SetFloat("Dissolve", matHold.health);
+            matHold.mat.SetColor("_Color", matHold.color);
+        }
+            
     }
 }
