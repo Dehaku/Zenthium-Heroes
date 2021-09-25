a03b8ca755df89376430ad3794a6f55fe7e8d0dd
Dehaku <dehakuzedon@gmail.com>
2021-09-25 11:13:58 -0500

    Rebinding basics

--- Assets/RebindingDisplay.cs
+++ Assets/RebindingDisplay.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RebindingDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] InputActionReference jumpAction = null;
+    [SerializeField] PlayerInput playerInput = null;
+    [SerializeField] TMP_Text bindingDisplayNameText = null;
+    [SerializeField] GameObject startRebindObject;
+    [SerializeField] GameObject waitingForInputObject;
+
+    InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+    public void StartRebinding()
     {
-        
+        startRebindObject.SetActive(false);
+        waitingForInputObject.SetActive(true);
+
+        playerInput.SwitchCurrentActionMap("Menu");
+
+        rebindingOperation = jumpAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation => RebindComplete())
+            .Start();
+
+
+
+
+
+
+        /*
+        jumpAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .OnComplete(callback =>
+            {
+                Debug.Log(callback.action.bindings[0].overridePath);
+                callback.Dispose();
+                playerInput.SwitchCurrentActionMap("Player");
+            })
+            .Start();
+        */
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void RebindComplete()
     {
-        
+        rebindingOperation.Dispose();
+
+        startRebindObject.SetActive(true);
+        waitingForInputObject.SetActive(false);
+
+        playerInput.SwitchCurrentActionMap("Player");
     }
 }
