cd45eaacf471c17b516cb66d69897e95522fb77a
Dehaku <dehakuzedon@gmail.com>
2021-09-27 13:28:01 -0500

    Reset All Rebinds button

--- Assets/ResetAllBindings.cs
+++ Assets/ResetAllBindings.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ResetAllBindings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    InputActionAsset inputActions;
+    public void ResetBindings()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (InputActionMap map in inputActions.actionMaps)
+        {
+            map.RemoveAllBindingOverrides();
+        }
     }
 }
