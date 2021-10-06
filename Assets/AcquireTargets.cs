f119339f3cd2ed60b8bce0eab077481fce46643b
Dehaku <dehakuzedon@gmail.com>
2021-10-06 11:22:55 -0500

    AcquireTarget distance checks, Turret displays range gizmo, turret min/max range

--- Assets/AcquireTargets.cs
+++ Assets/AcquireTargets.cs
@@ -7,6 +7,7 @@ public class AcquireTargets : MonoBehaviour
     public List<int> TargetFactions;
     
     [HideInInspector] public GameObject target;
+    [HideInInspector] public List<GameObject> targets;
 
 
     // Start is called before the first frame update
@@ -23,7 +24,7 @@ public class AcquireTargets : MonoBehaviour
         
     }
 
-    public GameObject AcquireNearestTarget()
+    public GameObject AcquireNearestEnemyTarget()
     {
         var pawns = FindObjectsOfType<Faction>();
         GameObject closestGO = null;
@@ -49,5 +50,72 @@ public class AcquireTargets : MonoBehaviour
         }
 
         return closestGO;
+    }
+
+    bool WithinRange(Vector3 myPos, Vector3 itPos, float minRange, float maxRange)
+    {
+        float dist = Vector3.Distance(myPos, itPos);
+        if (dist > minRange && dist < maxRange)
+            return true;
+
+        return false;
+    }
+
+    bool WithinRange(GameObject target, float minRange, float maxRange)
+    {
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        if (dist > minRange && dist < maxRange)
+            return true;
+
+        return false;
+    }
+
+    public GameObject AcquireNearestEnemyTargetWithinRange(float minRange, float maxRange)
+    {
+        return AcquireNearestEnemyTargetWithinRange(transform.position, minRange, maxRange);
+    }
+
+
+    public GameObject AcquireNearestEnemyTargetWithinRange(Vector3 searchPoint, float minRange, float maxRange)
+    {
+        var pawns = FindObjectsOfType<Faction>();
+        GameObject closestGO = null;
+        float closestDist = float.PositiveInfinity;
+
+        foreach (var pawn in pawns)
+        {
+            if (TargetFactions.Contains(pawn.GetFactionID()))
+            {
+                if (!closestGO && WithinRange(pawn.gameObject,minRange,maxRange))
+                {
+                    closestGO = pawn.gameObject;
+                    closestDist = Vector3.Distance(transform.position, closestGO.transform.position);
+                    continue;
+                }
+
+                if (Vector3.Distance(transform.position, pawn.transform.position) < closestDist && WithinRange(pawn.gameObject, minRange, maxRange))
+                {
+                    closestGO = pawn.gameObject;
+                    closestDist = Vector3.Distance(transform.position, closestGO.transform.position);
+                }
+            }
+        }
+
+        return closestGO;
+    }
+
+    public List<GameObject> AcquireAllEnemyTargets()
+    {
+        List<GameObject> targetList = new List<GameObject>();
+        var pawns = FindObjectsOfType<Faction>();
+
+        foreach (var pawn in pawns)
+        {
+            if (TargetFactions.Contains(pawn.GetFactionID()))
+            {
+                targetList.Add(pawn.gameObject);
+            }
+        }
+        return targetList;
     }
 }
--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -7,18 +7,30 @@ public class Turret : MonoBehaviour
     public AcquireTargets acquireTargets;
     public ShootKOBullet weaponScript;
     public GameObject firePoint;
+    
+    [Header("Functions")]
     public bool AllowedToShoot = false;
-    public Vector3 Offset;
+    public float minRange = 3f;
+    public float maxRange = 25f;
 
-    public float gravityOffset = 0;
-    public float speedOffset = 0;
+    
+    //public float gravityOffset = 0;
+    //public float speedOffset = 0;
 
+    [Header("Turret Parts")]
     public bool rotateParts = true;
     public Transform rotationHelper;
     public Transform rotSpin;
     public Transform rotVert;
     public Transform rotRotary;
-    public Vector3 offset;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, minRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxRange);
+    }
 
 
     // Start is called before the first frame update
@@ -29,18 +41,45 @@ public class Turret : MonoBehaviour
 
     public bool alter;
 
+
+    
+
+    bool WithinRange(Vector3 myPos, Vector3 itPos)
+    {
+        float dist = Vector3.Distance(myPos, itPos);
+        if (dist > minRange && dist < maxRange)
+            return true;
+
+        return false;
+    }
+
+    bool WithinRange(GameObject target)
+    {
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        if (dist > minRange && dist < maxRange)
+            return true;
+
+        return false;
+    }
+
+    
+
     // Update is called once per frame
     void Update()
     {
-        acquireTargets.target = acquireTargets.AcquireNearestTarget();
-        if(acquireTargets.target)
+        //acquireTargets.target = acquireTargets.AcquireNearestEnemyTarget();
+        acquireTargets.target = acquireTargets.AcquireNearestEnemyTargetWithinRange(minRange,maxRange);
+
+
+
+        if (acquireTargets.target)
         {
             if(rotateParts)
             {
                 rotationHelper.transform.LookAt(acquireTargets.target.transform.position + new Vector3(0, 1, 0));
                 
                 rotSpin.localEulerAngles = new Vector3(0, 0, rotationHelper.localEulerAngles.y);
-                rotVert.localEulerAngles = new Vector3(0+offset.x, 90+offset.y, 90+ (-rotationHelper.localEulerAngles.x)+offset.z);
+                rotVert.localEulerAngles = new Vector3(0, 90, 90+ (-rotationHelper.localEulerAngles.x));
                 
                 firePoint.transform.rotation = rotationHelper.rotation;
             }
@@ -51,19 +90,27 @@ public class Turret : MonoBehaviour
             
         }
 
-        //Vector3 solve1 = new Vector3();
-        //Vector3 solve2 = new Vector3();
-        //int solveIndex = fts.solve_ballistic_arc(weaponScript.spawnPos.position, weaponScript.bulletSpeed+speedOffset,
-        //    acquireTargets.target.transform.position, weaponScript.bulletGravity+gravityOffset, out solve1, out solve2);
-        //float solve1;
-        //float solve2;
-        //int solveIndex = fts.solve_ballistic_arc_ang(weaponScript.spawnPos.position, weaponScript.bulletSpeed + speedOffset,
-        //    acquireTargets.target.transform.position, weaponScript.bulletGravity+gravityOffset, out solve1, out solve2);
-        //
-        //solve1 = solve1 * weaponScript.bulletSpeed + solve1 * weaponScript.bulletSpeed;
-        //solve2 *= weaponScript.bulletSpeed;
-        //
-        //Debug.Log(solveIndex + "    :   " + solve1 + "   :   " + solve1 * weaponScript.bulletGravity);
+
+        { // Failed arc calculation.
+
+            //Vector3 solve1 = new Vector3();
+            //Vector3 solve2 = new Vector3();
+            //int solveIndex = fts.solve_ballistic_arc(weaponScript.spawnPos.position, weaponScript.bulletSpeed+speedOffset,
+            //    acquireTargets.target.transform.position, weaponScript.bulletGravity+gravityOffset, out solve1, out solve2);
+            //float solve1;
+            //float solve2;
+            //int solveIndex = fts.solve_ballistic_arc_ang(weaponScript.spawnPos.position, weaponScript.bulletSpeed + speedOffset,
+            //    acquireTargets.target.transform.position, weaponScript.bulletGravity+gravityOffset, out solve1, out solve2);
+            //
+            //solve1 = solve1 * weaponScript.bulletSpeed + solve1 * weaponScript.bulletSpeed;
+            //solve2 *= weaponScript.bulletSpeed;
+            //
+            //Debug.Log(solveIndex + "    :   " + solve1 + "   :   " + solve1 * weaponScript.bulletGravity);
+
+        }
+
+
+
 
 
 
@@ -76,9 +123,9 @@ public class Turret : MonoBehaviour
         }
         if(alter)
         {
-            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x + Offset.x,
-                transform.rotation.eulerAngles.y + Offset.y,
-                transform.rotation.eulerAngles.z + Offset.z);
+            transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y,
+                transform.rotation.eulerAngles.z);
         }
 
 
