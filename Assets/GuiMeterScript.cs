f50e761295bf26b4fdeaf75ea96013f834da8222
Dehaku <dehakuzedon@gmail.com>
2021-10-09 15:14:05 -0500

    CircleHealthbar shader

---
+++ Assets/GuiMeterScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiMeterScript : MonoBehaviour
+{
+    public float value = 100;
+    public float maxValue = 100;
+    public float segments = 4;
+    public float segmentSpacing = 0.01f;
+
+    List<Material> meters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
