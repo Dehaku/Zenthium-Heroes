62b3aabc2ade3c6bf2874846c0f647a29cf85437
Dehaku <dehakuzedon@gmail.com>
2022-03-19 20:19:38 -0500

    Fixed squad villain target crash & camera boundry

--- Assets/ChaseObserveTarget.cs
+++ Assets/ChaseObserveTarget.cs
@@ -4,114 +4,113 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    [RequireComponent(typeof(ChaseTarget))]
-    public class ChaseObserveTarget : MonoBehaviour
+[RequireComponent(typeof(ChaseTarget))]
+public class ChaseObserveTarget : MonoBehaviour
+{
+    ChaseTarget _chaseT;
+    SquadScript squad;
+    AcquireTargets acquire;
+    public float scanMinRange = 0f;
+    public float scanMaxRange = 50f;
+    public float scanFrequency = 1.5f;
+    [Header("ScanRandom: No Less Than Freq")]
+    public float scanRandomness = 0.25f;
+    float _scanTimer = 1.5f;
+    public Transform sightPos;
+
+    private void Awake()
     {
-        ChaseTarget _chaseT;
-        SquadScript squad;
-        AcquireTargets acquire;
-        public float scanMinRange = 0f;
-        public float scanMaxRange = 50f;
-        public float scanFrequency = 1.5f;
-        [Header("ScanRandom: No Less Than Freq")]
-        public float scanRandomness = 0.25f;
-        float _scanTimer = 1.5f;
-        public Transform sightPos;
-
-        private void Awake()
-        {
-            _chaseT = GetComponent<ChaseTarget>();
+        _chaseT = GetComponent<ChaseTarget>();
             
-        }
-        void Start()
-        {
-            squad = GetComponent<SquadRef>().squad;
-            acquire = GetComponent<AcquireTargets>();
-        }
+    }
+    void Start()
+    {
+        squad = GetComponent<SquadRef>().squad;
+        acquire = GetComponent<AcquireTargets>();
+    }
 
-        void OnDrawGizmosSelected()
-        {
-            // Draw a yellow sphere at the transform's position
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
             
-            if(scanMinRange > 0)
-            {
-                Gizmos.color = new Color(0,0,1,1);
-                if (sightPos)
-                    Gizmos.DrawWireSphere(sightPos.position, scanMinRange);
-                else
-                    Gizmos.DrawWireSphere(transform.position, scanMinRange);
-            }
-            Gizmos.color = new Color(1, 0.92f, 0.016f,1); 
+        if(scanMinRange > 0)
+        {
+            Gizmos.color = new Color(0,0,1,1);
             if (sightPos)
-                Gizmos.DrawWireSphere(sightPos.position, scanMaxRange);
+                Gizmos.DrawWireSphere(sightPos.position, scanMinRange);
             else
-                Gizmos.DrawWireSphere(transform.position, scanMaxRange);
+                Gizmos.DrawWireSphere(transform.position, scanMinRange);
         }
+        Gizmos.color = new Color(1, 0.92f, 0.016f,1); 
+        if (sightPos)
+            Gizmos.DrawWireSphere(sightPos.position, scanMaxRange);
+        else
+            Gizmos.DrawWireSphere(transform.position, scanMaxRange);
+    }
 
-        void MakeTarget(GameObject target)
-        {
-            if (!target)
+    void MakeTarget(GameObject target)
+    {
+        if (!target)
+            return;
+        var targetCreature = target.GetComponent<Creature>();
+        if (!targetCreature)
+            return;
+        if (!targetCreature.isConscious)
             return;
 
-            var targetCreature = target.GetComponent<Creature>();
-            if (!targetCreature.isConscious)
-            {
-                return;
-            }
-
-            // If Squad, and stay in formation, then squad hunt. Else chase by yourself.
-            bool seekSolo = true;
-            if (GetSquad())
-            {
+        // If Squad, and stay in formation, then squad hunt. Else chase by yourself.
+        bool seekSolo = true;
+        if (GetSquad())
+        {
                 
-                //Setting target so melee units in formation will attack if enemies approach.
-                _chaseT.target = target;
+            //Setting target so melee units in formation will attack if enemies approach.
+            _chaseT.target = target;
 
             seekSolo = false;
             if (GetSquad().breakFormation)
-                    seekSolo = true;
-                else
-                    GetSquad().TargetFound(target);
-            }
-
-            if (seekSolo)
-            {
-                _chaseT.enabled = true;
-                _chaseT.target = target;
-            }
-        }
-
-        void Scan()
-        {
-            GameObject enemy;
-            if(sightPos)
-                enemy = acquire.AcquireNearestVisibleEnemyWithinRange(sightPos.position, scanMinRange, scanMaxRange);
+                seekSolo = true;
             else
-                enemy = acquire.AcquireNearestVisibleEnemyWithinRange(scanMinRange, scanMaxRange);
-
-            if (enemy)
-            {
-                MakeTarget(enemy);
-            }
+                GetSquad().TargetFound(target);
         }
 
-        void Update()
+        if (seekSolo)
         {
-            _scanTimer -= Time.deltaTime;
-            if(_scanTimer < 0)
-            {
-                _scanTimer = scanFrequency + UnityEngine.Random.Range(0f, scanRandomness);
-                Scan();
-            }
-        }
-
-
-        SquadScript GetSquad()
-        {
-            if (squad)
-                return squad;
-
-            squad = GetComponent<SquadRef>().squad;
-            return squad;
+            _chaseT.enabled = true;
+            _chaseT.target = target;
         }
     }
+
+    void Scan()
+    {
+        GameObject enemy;
+        if(sightPos)
+            enemy = acquire.AcquireNearestVisibleEnemyWithinRange(sightPos.position, scanMinRange, scanMaxRange);
+        else
+            enemy = acquire.AcquireNearestVisibleEnemyWithinRange(scanMinRange, scanMaxRange);
+        if (enemy)
+        {
+            MakeTarget(enemy);
+        }
+    }
+
+
+    void Update()
+    {
+        _scanTimer -= Time.deltaTime;
+        if(_scanTimer < 0)
+        {
+            _scanTimer = scanFrequency + UnityEngine.Random.Range(0f, scanRandomness);
+            Scan();
+        }
+    }
+
+
+    SquadScript GetSquad()
+    {
+        if (squad)
+            return squad;
+
+        squad = GetComponent<SquadRef>().squad;
+        return squad;
+    }
+}
