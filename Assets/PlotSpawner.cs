58b4ab6323baf5586c123cda3427b1aeee12f6f5
Dehaku <dehakuzedon@gmail.com>
2022-03-31 17:13:43 -0500

    Made medium plot, spawns smaller plots
    Generation and clearing now recursive.

--- Assets/PlotSpawner.cs
+++ Assets/PlotSpawner.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class PlotSpawner : MonoBehaviour
 {
     public PlotBuildingContainer PBC;
-    public GameObject myBuildings;
+    public GameObject myBuilding;
     [SerializeField] GameObject _plane;
+    [Range(0f, 100f)] public float chanceOfSubplots = 0;
+    [HideInInspector] public bool subPlotting = false;
+    public List<GameObject> myPlots = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         if(!PBC)
         {
-            Debug.LogError("Plot " + name + " does not have a PBC set.");
+            Debug.LogError(name + " does not have a PBC set.");
         }
             
         if(PBC.BuildingPrefabs.Length == 0)
         {
-            Debug.LogWarning("PlotBuildingContainer is empty!");
+            Debug.LogWarning(name + "PlotBuildingContainer is empty!");
             return;
         }
+
+        if(myPlots.Count == 0)
+        {
+            if(chanceOfSubplots != 0)
+            {
+                Debug.LogWarning(name + " myPlots are not filled, but chance of Subplots is above 0!");
+                return;
+            }
+        }
+
         Generate();
         
         _plane.SetActive(false);
@@ -29,27 +42,74 @@ public class PlotSpawner : MonoBehaviour
     [EButton.BeginHorizontal("Buttons"),EButton]
     public void Generate()
     {
-        if(myBuildings)
+        if(myBuilding)
         {
             Debug.Log("There's already something here!");
             return;
         }
             
+        if(chanceOfSubplots > 0 || subPlotting)
+        {
+            float rolledChance = Random.Range(0f, 100f);
+            Debug.Log("Rolled: " + rolledChance + " vs " + chanceOfSubplots);
+
+            if(rolledChance <= chanceOfSubplots)
+            {
+                foreach (var subPlot in myPlots)
+                {
+                    subPlot.SetActive(true);
+                    subPlot.GetComponent<PlotSpawner>().Generate();
+                    subPlotting = true;
+                }
+                
+                return;
+            }
+        }
+
         GameObject item = PBC.BuildingPrefabs[Random.Range(0, PBC.BuildingPrefabs.Length)];
-        myBuildings = Instantiate(item, transform.position, transform.rotation, transform);
+        myBuilding = Instantiate(item, transform.position, transform.rotation, transform);
     }
 
     [EButton("Clear")]
     public void Clear()
     {
-        if (myBuildings)
-            Destroy(myBuildings);
+        if(subPlotting)
+        {
+            foreach (var subPlot in myPlots)
+            {
+                subPlot.GetComponent<PlotSpawner>().Clear();
+                subPlot.SetActive(false);
+                subPlotting = false;
+            }
+        }
+        else if (myBuilding)
+        {
+            Destroy(myBuilding);
+            myBuilding = null;
+        }
+            
     }
 
-    // Update is called once per frame
-    void Update()
+    [EButton("Clear*")]
+    public void ClearInstant()
     {
-        
+        if (subPlotting)
+        {
+            foreach (var subPlot in myPlots)
+            {
+                subPlot.GetComponent<PlotSpawner>().ClearInstant();
+                subPlot.SetActive(false);
+                subPlotting = false;
+            }
+        }
+        else if (myBuilding)
+        {
+            DestroyImmediate(myBuilding);
+            myBuilding = null;
+        }
+            
     }
+
+    
     
 }
