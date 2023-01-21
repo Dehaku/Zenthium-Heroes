9353a41b16ea93145fc413dc63be3d89b2996f84
Dehaku <dehakuzedon@gmail.com>
2023-01-21 13:08:39 -0600

    Found source of jank rb on rb movement

---
+++ Assets/Scripts/DaveGD/RBMovingObjectSensor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+
+namespace PhysicsBasedCharacterController
+{
+    [RequireComponent(typeof(Collider))]
+    public class RBMovingObjectSensor : MonoBehaviour
+    {
+        public RBMovingObject rbMovingObject;
+        private BoxCollider boxCollider;
+
+
+        /**/
+
+
+        private void Awake()
+        {
+            //rbMovingObject = this.transform.parent.GetComponent<RBMovingObject>();
+            if (!rbMovingObject)
+                Debug.LogError(transform.parent.name + "No rbMovingObject assigned!");
+        }
+
+
+        #region Collision detection
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            if (rigidbody != null && rigidbody != rbMovingObject.GetComponent<Rigidbody>())
+            {
+                rbMovingObject.Add(rigidbody);
+                return;
+            }
+            //if(other.attachedRigidbody != null && other.attachedRigidbody != movingPlatform.GetComponent<Rigidbody>()) movingPlatform.Add(rigidbody);
+            Debug.Log("Trigger In: " + other.attachedRigidbody);
+            if (other.attachedRigidbody)
+            {
+                rbMovingObject.Add(other.attachedRigidbody);
+                Debug.Log("Name of : " + other.attachedRigidbody.name);
+            }
+                
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+            if (rigidbody != null && rigidbody != rbMovingObject.GetComponent<Rigidbody>())
+            {
+                rbMovingObject.Remove(rigidbody);
+                return;
+            }
+            if (other.attachedRigidbody)
+            {
+                rbMovingObject.Remove(other.attachedRigidbody);
+                return;
+            }
+        }
+
+        #endregion
+    }
+}
