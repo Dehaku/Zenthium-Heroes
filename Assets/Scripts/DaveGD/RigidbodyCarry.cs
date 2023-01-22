cf8b1e90a832cd7f2ce82b6cc29fc893bd8443be
Dehaku <dehakuzedon@gmail.com>
2023-01-22 16:53:52 -0600

    Streamlined Horizontal scans

--- Assets/Scripts/DaveGD/RigidbodyCarry.cs
+++ Assets/Scripts/DaveGD/RigidbodyCarry.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class RigidbodyCarry : MonoBehaviour
 {
 
-    public bool allowNonRigidbodyCarriersToo = false;
     public bool keepMomentumOnExitCollision = true;
     public bool keepRotationMomentumOnExitCollision = true;
 
@@ -20,14 +19,16 @@ public class RigidbodyCarry : MonoBehaviour
 
     [Header("Raycast Or Collision")]
     public bool useRayCastInsteadOfCollision = false;
-    public bool useSidewaysRays = false;
-    public bool useFrontAndBackRays = false;
-    public float rayCastLength = 1f;
+    public bool allowHorizontalRays = false;
+    public float downRayCastLength = 1f;
+    public float horizontalRayCastLength = 1f;
     public LayerMask carrierLayerMasks;
     public Transform alternateCastPosition;
 
     [Space(5)]
     public bool drawDebugRaycastLines = false;
+
+    Vector3[] vectorDirections;
 
 
     // Start is called before the first frame update
@@ -38,7 +39,28 @@ public class RigidbodyCarry : MonoBehaviour
         if (!_myTransform)
             Debug.LogError("No Transform assigned");
 
-        Debug.Log("Swap to allow nonrigidbody carriers for COLLISION too");
+        // Filling in vector positions for Horizontal checks. Added vertical ones for fun.
+        vectorDirections = new Vector3[10];
+        // Down
+        vectorDirections[0] = new Vector3(0, -1, 0);
+        // Left
+        vectorDirections[1] = new Vector3(-1, 0, 0);
+        // ForwardLeft
+        vectorDirections[2] = new Vector3(-1, 0, 1);
+        // Forward
+        vectorDirections[3] = new Vector3(0, 0, 1);
+        // ForwardRight
+        vectorDirections[4] = new Vector3(1, 0, 1);
+        // Right
+        vectorDirections[5] = new Vector3(1, 0, 0);
+        // RightBack
+        vectorDirections[6] = new Vector3(1, 0, -1);
+        // Back
+        vectorDirections[7] = new Vector3(0, 0, -1);
+        // BackLeft
+        vectorDirections[8] = new Vector3(-1, 0, -1);
+        // Up
+        vectorDirections[9] = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
@@ -94,23 +116,15 @@ public class RigidbodyCarry : MonoBehaviour
     {
         if (keepMomentumOnExitCollision && _myRB.velocity.magnitude > 0)
             _myRB.velocity += _rb.velocity;
-        Debug.Log("Add third velocity tracker to allow for transform based exits");
         
         _carrierRB = null;
     }
 
 
+
+
     private void RaycastChecks()
     {
-
-        /*
-          public bool useRayCastInsteadOfCollision = false;
-          public bool useSidewaysRays = false;
-          public bool useFrontAndBackRays = false;
-          public float rayCastLength = 1f;
-          public LayerMask carrierLayerMasks;
-          public Transform alternateCastPosition;
-        */
 
         Vector3 castPos = transform.position;
         Vector3 castForward = transform.forward;
@@ -130,7 +144,7 @@ public class RigidbodyCarry : MonoBehaviour
         // =====Down=====
         // Downcast should be universal, if not, feel free to add a bool here.
         Physics.Raycast(castPos, -castUp,
-                            out raycastHit, rayCastLength, carrierLayerMasks);
+                            out raycastHit, downRayCastLength, carrierLayerMasks);
 
         if (raycastHit.collider)
             anyHits = RaycastHit(raycastHit.collider);
@@ -138,18 +152,21 @@ public class RigidbodyCarry : MonoBehaviour
         if (drawDebugRaycastLines)
         {
             if(raycastHit.collider)
-                Debug.DrawLine(castPos, raycastHit.point, Color.white);
+                Debug.DrawLine(castPos, raycastHit.point, Color.green);
             else
-                Debug.DrawLine(castPos, castPos + (-castUp * rayCastLength), Color.green);
+                Debug.DrawLine(castPos, castPos + (-castUp * downRayCastLength), Color.red);
         }
 
-        // =====Sideways=====
+        // =====Horizontal=====
         // If down didn't hit, let's cast around us if allowed.
-        if(useSidewaysRays && anyHits == false)
+        for(int i = 1; i < vectorDirections.Length; i++) // Start at 1, since 0 has down.
         {
-            // Right Cast
-            Physics.Raycast(castPos, castRight,
-                                out raycastHit, rayCastLength, carrierLayerMasks);
+            //Making sure we're allowed. If we are, making sure we don't have any hits.
+            if (!allowHorizontalRays || anyHits)
+                break;
+
+            Physics.Raycast(castPos, vectorDirections[i],
+                                out raycastHit, horizontalRayCastLength, carrierLayerMasks);
 
             if (raycastHit.collider)
                 anyHits = RaycastHit(raycastHit.collider);
@@ -157,71 +174,11 @@ public class RigidbodyCarry : MonoBehaviour
             if (drawDebugRaycastLines)
             {
                 if (raycastHit.collider)
-                    Debug.DrawLine(castPos, raycastHit.point, Color.white);
+                    Debug.DrawLine(castPos, raycastHit.point, Color.green);
                 else
-                    Debug.DrawLine(castPos, castPos + (castRight * rayCastLength), Color.red);
-            }
-
-            // Left Cast
-            if(anyHits == false)
-            {
-                Physics.Raycast(castPos, -castRight,
-                                out raycastHit, rayCastLength, carrierLayerMasks);
-
-                if (raycastHit.collider)
-                    anyHits = RaycastHit(raycastHit.collider);
-
-                if (drawDebugRaycastLines)
-                {
-                    if (raycastHit.collider)
-                        Debug.DrawLine(castPos, raycastHit.point, Color.white);
-                    else
-                        Debug.DrawLine(castPos, castPos + (-castRight * rayCastLength), Color.red);
-                }
+                    Debug.DrawLine(castPos, castPos + (vectorDirections[i] * horizontalRayCastLength), Color.red);
             }
         }
-
-        // =====Front and Back=====
-        // If down AND sides didn't hit, let's cast front and back if allowed.
-        if (useFrontAndBackRays && anyHits == false)
-        {
-            // Forward Cast
-            Physics.Raycast(castPos, castForward,
-                                out raycastHit, rayCastLength, carrierLayerMasks);
-
-            if (raycastHit.collider)
-                anyHits = RaycastHit(raycastHit.collider);
-
-            if (drawDebugRaycastLines)
-            {
-                if (raycastHit.collider)
-                    Debug.DrawLine(castPos, raycastHit.point, Color.white);
-                else
-                    Debug.DrawLine(castPos, castPos + (castForward * rayCastLength), Color.blue);
-            }
-
-            // Back Cast
-            if (anyHits == false)
-            {
-                Physics.Raycast(castPos, -castForward,
-                                out raycastHit, rayCastLength, carrierLayerMasks);
-
-                if (raycastHit.collider)
-                    anyHits = RaycastHit(raycastHit.collider);
-
-                if (drawDebugRaycastLines)
-                {
-                    if (raycastHit.collider)
-                        Debug.DrawLine(castPos, raycastHit.point, Color.white);
-                    else
-                        Debug.DrawLine(castPos, castPos + (-castForward * rayCastLength), Color.blue);
-                }
-            }
-        }
-
-
-
-
 
         if (anyHits == false)
         {
@@ -235,12 +192,6 @@ public class RigidbodyCarry : MonoBehaviour
     bool RaycastHit(Collider collider)
     {
         bool anyHits = false;
-        // Do we allow moving objects without rigidbodies?
-        if (allowNonRigidbodyCarriersToo)
-        {
-            anyHits = true;
-            Debug.Log("Not implemented yet");
-        }
 
         // Assign hit rigidbody as our carrier
         Rigidbody _rb = collider.attachedRigidbody;
