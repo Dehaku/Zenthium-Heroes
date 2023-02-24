ece4c4ff3f0950cbe1f9635037a4f12a188dc72a
Dehaku <dehakuzedon@gmail.com>
2023-02-24 12:07:00 -0600

    Two character animation setup

--- Assets/AnimationSyncer.cs
+++ Assets/AnimationSyncer.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class AnimationSyncer : MonoBehaviour
 {
 
-    public List<Animator> animators;
-    public AnimationClip clip;
+    public List<Animator> animators0;
+    public AnimationClip clip0;
+    public List<Animator> animators1;
+    public AnimationClip clip1;
     public bool playOnStart = true;
+    public bool playNow = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +22,23 @@ public class AnimationSyncer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(playNow)
+        {
+            playNow = false;
+            PlayAnimation();
+        }
     }
 
     [ContextMenu("PlayAll")]
     void PlayAnimation()
     {
-        foreach (var item in animators)
+        foreach (var item in animators0)
         {
-            item.Play(clip.name);
+            item.Play(clip0.name);
+        }
+        foreach (var item in animators1)
+        {
+            item.Play(clip1.name);
         }
     }
 }
